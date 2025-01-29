adbd520a42184cdfb7e64d82a7cc675b706f5e52
Prosper <prosperorjiogo1999@gmail.com>
2025-01-29 11:53:47 +0100

    added port env value

--- app.js
+++ app.js
@@ -63,4 +63,5 @@ function validateCourse(course) {
     return schema.validate(course);
 }
 
-app.listen(3000, () => console.log('Listening on port 3000...'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
